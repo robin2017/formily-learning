17ddb1408f6ecc82fa53a90ef32c4ca9e9fc7f3c
wangy202012 <1143027179@qq.com>
2021-02-06 13:55:58 +0800

    first demo

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,33 @@
-import React from 'react';
-export default ()=>{
-    return (
-        <div>hello,formily</div>
-    )
+import React from 'react'
+import { SchemaForm, FormButtonGroup, Submit } from '@formily/next'
+import { Input } from '@formily/next-components'
+import '@alifd/next/dist/next.css'
+
+export default () => {
+  return (
+    <SchemaForm
+      components={{
+        Input
+      }}
+      onSubmit={console.log}
+      schema={{
+        type: 'object',
+        'x-component-props': {
+          labelCol: 7,
+          wrapperCol: 12
+        },
+        properties: {
+          string: {
+            type: 'string',
+            title: 'String',
+            'x-component': 'Input'
+          }
+        }
+      }}
+    >
+      <FormButtonGroup offset={7}>
+        <Submit>提交</Submit>
+      </FormButtonGroup>
+    </SchemaForm>
+  )
 }
