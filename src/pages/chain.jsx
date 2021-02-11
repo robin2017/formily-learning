10aa7e02abb813716651e5a8b21b81f2d5457c3b
wangy202012 <1143027179@qq.com>
2021-02-11 23:39:57 +0800

    suc

--- src/pages/chain.jsx
+++ src/pages/chain.jsx
@@ -1,51 +1,41 @@
 import React, { useEffect, useState } from 'react';
-
 import {
   SchemaForm,
   SchemaMarkupField as Field,
   FormButtonGroup,
-  FormEffectHooks,
   Submit,
   Reset,
-} from '@formily/next'; // 或者 @formily/next
-import Printer from '@formily/printer';
-import { merge } from 'rxjs';
-import { Input, Select } from '@formily/next-components'; // 或者@formily/next-components
-
+} from '@formily/next';
+import { Input, Select } from '@formily/next-components';
 const components = {
   Input,
   Select,
 };
-
-const { onFieldValueChange$, onFieldInit$ } = FormEffectHooks;
+const aaOptions = [
+  { label: '123', value: 123 },
+  { label: '321', value: 321 },
+];
 
 export default () => {
   return (
     <SchemaForm
+      onSubmit={console.log}
       labelCol={5}
       wrapperCol={14}
       components={components}
-      effects={({ setFieldState }) => {
-        merge(onFieldValueChange$('aa'), onFieldInit$('aa')).subscribe(
-          (fieldState) => {
-            setFieldState('bb', (state) => {
-              state.visible = fieldState.value === 123;
-            });
-          }
-        );
+      // 控制显示隐藏
+      effects={($, { setFieldState }) => {
+        // $为函数:function (type, matcher)
+        $('onFieldChange', 'aa').subscribe((fieldState) => {
+          console.log('aa field-change:', fieldState);
+          setFieldState('bb', (state) => {
+            state.visible = fieldState.value === 123;
+          });
+        });
       }}
     >
-      <Field
-        type='string'
-        title='AA'
-        enum={[
-          { label: '123', value: 123 },
-          { label: '321', value: 321 },
-        ]}
-        name='aa'
-        x-component='Select'
-      />
-      <Field type='string' title='BB' name='bb' x-component='Input' />
+      <Field title='AA' enum={aaOptions} name='aa' x-component='Select' />
+      <Field title='BB' name='bb' x-component='Input' />
       <FormButtonGroup offset={5}>
         <Submit>查询</Submit>
         <Reset>重置</Reset>
